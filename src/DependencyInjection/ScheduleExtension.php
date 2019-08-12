59c369a875210569de1380dd8276204019771ee7
natepage <nathan.page86@gmail.com>
2019-08-12 12:32:43 +0800

    add services config

--- src/DependencyInjection/ScheduleExtension.php
+++ src/DependencyInjection/ScheduleExtension.php
@@ -4,16 +4,23 @@ declare(strict_types=1);
 namespace LoyaltyCorp\Schedule\ScheduleBundle\DependencyInjection;
 
 use LoyaltyCorp\Schedule\ScheduleBundle\Interfaces\ScheduleProviderInterface;
+use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Extension\Extension;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 
 final class ScheduleExtension extends Extension
 {
     /**
      * {@inheritDoc}
+     *
+     * @throws \Exception
      */
     public function load(array $configs, ContainerBuilder $container)
     {
+        $loader = new YamlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
+        $loader->load('services.yaml');
+
         $container
             ->registerForAutoconfiguration(ScheduleProviderInterface::class)
             ->addTag(ScheduleProviderInterface::TAG_SCHEDULE_PROVIDER);
